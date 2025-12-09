fdae473263b538f353df2506d0fdaa9d68eb2522
Daniel-Sameh <111789605+Daniel-Sameh@users.noreply.github.com>
2025-12-09 20:10:23 +0200

    Project Init

---
+++ src/config/settings.py
@@ -0,0 +1,5 @@
+
+class Settings:
+    def __init__(self):
+        self.debug = True
+        self.classes= ['cardboard', 'glass', 'metal', 'paper', 'plastic', 'trash', 'unknown']
---
+++ src/data/data_loader.py
@@ -0,0 +1,11 @@
+from src.config.settings import Settings
+from typing import List, Str
+from sklearn.model_selection import train_test_split
+
+class DataLoader:
+    def __init__(self, settings: Settings):
+        self.settings = Settings()
+
+
+    def load_data(self, source: Str):
+        pass
